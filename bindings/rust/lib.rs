357d65a697fd8a0eaa0d02cdaac5f20f469630f1
belltoy <belltoy@gmail.com>
2024-07-27 23:20:41 +0800

    Add injections.scm and indents.scm queries

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -38,7 +38,9 @@ pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 // Uncomment these to include any queries that this grammar contains
 
 pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+pub const INDENTS_QUERY: &str = include_str!("../../queries/indents.scm");
+pub const FOLDS_QUERY: &str = include_str!("../../queries/folds.scm");
 // pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
 // pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
